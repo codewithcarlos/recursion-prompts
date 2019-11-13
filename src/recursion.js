03d7b94da1dc769c042b797420a3fb12c19ec53e
Carlos Munoz <42192364+codewithcarlos@users.noreply.github.com>
2019-11-13 13:18:28 -0800

    Complete modulo

--- src/recursion.js
+++ src/recursion.js
@@ -131,7 +131,7 @@ var palindrome = function (string) {
     return true;
   }
   if (string[0] === string[string.length - 1]) {
-    return palindrome(string.substring(1, string.length-1));
+    return palindrome(string.substring(1, string.length - 1));
   } else {
     return false;
   }
@@ -142,7 +142,14 @@ var palindrome = function (string) {
 // modulo(5,2) // 1
 // modulo(17,5) // 2
 // modulo(22,6) // 4
-var modulo = function (x, y) { };
+// modulo(-4, -2) //0
+var modulo = function (x, y) {
+  if (y === 0) return NaN;
+  if (x < 0) return -modulo(-x, y);
+  if (y < 0) return modulo(x, -y);
+  if (x < y) return x;
+  return modulo(x - y, y);
+};
 
 // 12. Write a function that multiplies two numbers without using the * operator or
 // Math methods.
